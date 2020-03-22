e39c487afd5c83b21d9333ad98bc62fe88444c79
Alfred Qiu <sc941203@gmail.com>
2020-03-22 18:12:03 +0800

    0.1.0

--- src/.umi/core/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '/Users/alfred/Desktop/dvp/chaos-hooks/node_modules/_@umijs_runtime@3.0.12@@umijs/runtime/dist/index.js';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
